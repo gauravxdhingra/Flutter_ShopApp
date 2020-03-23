a3014cbb7ca52a5b08c160ebe44959c76caf0097
GauravXd <grvdhingra1999@gmail.com>
2020-03-23 05:36:28 +0530

    Products overview Screen added

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,19 @@
 import 'package:flutter/material.dart';
 
+import './screens/products_overview_screen.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'My Shop',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('widget.title'),
-      ),
-      body: Center(),
+      home: ProductsOverviewScreen(),
     );
   }
 }

c6934aa11bb9e6bfe5ccf2c1731cc6917a259a9b
GauravXd <grvdhingra1999@gmail.com>
2020-03-25 23:38:28 +0530

    Updated

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -16,7 +16,7 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final product = Provider.of<Product>(context);
+    final product = Provider.of<Product>(context, listen: false);
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
@@ -42,13 +42,15 @@ class ProductItem extends StatelessWidget {
         footer: GridTileBar(
           leading: Consumer<Product>(
             builder: (ctx, product, child) => IconButton(
-                icon: Icon(
-                  product.isFav ? Icons.favorite : Icons.favorite_border,
-                  color: Theme.of(context).accentColor,
-                ),
-                onPressed: () {
-                  product.toggleFavStatus();
-                }),
+              icon: Icon(
+                product.isFav ? Icons.favorite : Icons.favorite_border,
+                color: Theme.of(context).accentColor,
+              ),
+              onPressed: () {
+                product.toggleFavStatus();
+              },
+            ),
+            // child: ,
           ),
           backgroundColor: Colors.black87,
           title: Text(
